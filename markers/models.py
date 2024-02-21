75b2641972ec15e9c02e567009c2eec833433090
jorniklenderlyn <khcfksiirjfldik@gmail.com>
2024-02-22 05:39:51 +1000

    edit table db

--- markers/models.py
+++ markers/models.py
@@ -100,8 +100,75 @@ class y2019_Evi_20_1(models.Model):
     class Meta:
         managed = False
         db_table = '2019_EVI_20_1'
-        unique_together = (('x', 'y'),)
+        unique_together = (('x', 'y'))
 
+
+class y2019_Ndvi_10_1(models.Model):
+    geom = models.PointField(blank=True, null=True)
+    x = models.FloatField(db_column='X')  # Field name made lowercase.
+    y = models.FloatField(db_column='Y')  # Field name made lowercase.
+    ndv1 = models.FloatField(db_column='NDV1')  # Field name made lowercase.
+    ndv2 = models.FloatField(db_column='NDV2')  # Field name made lowercase.
+    ndv3 = models.FloatField(db_column='NDV3')  # Field name made lowercase.
+    ndv4 = models.FloatField(db_column='NDV4')  # Field name made lowercase.
+    ndv5 = models.FloatField(db_column='NDV5')  # Field name made lowercase.
+    ndv6 = models.FloatField(db_column='NDV6')  # Field name made lowercase.
+    ndv7 = models.FloatField(db_column='NDV7')  # Field name made lowercase.
+    ndv8 = models.FloatField(db_column='NDV8')  # Field name made lowercase.
+    ndv9 = models.FloatField(db_column='NDV9')  # Field name made lowercase.
+    ndv10 = models.FloatField(db_column='NDV10')  # Field name made lowercase.
+    ndv11 = models.FloatField(db_column='NDV11')  # Field name made lowercase.
+    ndv12 = models.FloatField(db_column='NDV12')  # Field name made lowercase.
+    ndv13 = models.FloatField(db_column='NDV13')  # Field name made lowercase.
+    ndv14 = models.FloatField(db_column='NDV14')  # Field name made lowercase.
+    ndv15 = models.FloatField(db_column='NDV15')  # Field name made lowercase.
+    ndv16 = models.FloatField(db_column='NDV16')  # Field name made lowercase.
+    ndv17 = models.FloatField(db_column='NDV17')  # Field name made lowercase.
+    ndv18 = models.FloatField(db_column='NDV18')  # Field name made lowercase.
+    ndv19 = models.FloatField(db_column='NDV19')  # Field name made lowercase.
+    ndv20 = models.FloatField(db_column='NDV20')  # Field name made lowercase.
+    ndv21 = models.FloatField(db_column='NDV21')  # Field name made lowercase.
+    ndv22 = models.FloatField(db_column='NDV22')  # Field name made lowercase.
+    ndv23 = models.FloatField(db_column='NDV23')  # Field name made lowercase.
+    ndv24 = models.FloatField(db_column='NDV24')  # Field name made lowercase.
+    ndv25 = models.FloatField(db_column='NDV25')  # Field name made lowercase.
+    ndv26 = models.FloatField(db_column='NDV26')  # Field name made lowercase.
+    ndv27 = models.FloatField(db_column='NDV27')  # Field name made lowercase.
+    ndv28 = models.FloatField(db_column='NDV28')  # Field name made lowercase.
+    ndv29 = models.FloatField(db_column='NDV29')  # Field name made lowercase.
+    ndv30 = models.FloatField(db_column='NDV30')  # Field name made lowercase.
+    ndv31 = models.FloatField(db_column='NDV31')  # Field name made lowercase.
+    ndv32 = models.FloatField(db_column='NDV32')  # Field name made lowercase.
+    ndv33 = models.FloatField(db_column='NDV33')  # Field name made lowercase.
+    ndv34 = models.FloatField(db_column='NDV34')  # Field name made lowercase.
+    ndv35 = models.FloatField(db_column='NDV35')  # Field name made lowercase.
+    ndv36 = models.FloatField(db_column='NDV36')  # Field name made lowercase.
+    ndv37 = models.FloatField(db_column='NDV37')  # Field name made lowercase.
+    ndv38 = models.FloatField(db_column='NDV38')  # Field name made lowercase.
+    ndv39 = models.FloatField(db_column='NDV39')  # Field name made lowercase.
+    ndv40 = models.FloatField(db_column='NDV40')  # Field name made lowercase.
+    ndv41 = models.FloatField(db_column='NDV41')  # Field name made lowercase.
+    ndv42 = models.FloatField(db_column='NDV42')  # Field name made lowercase.
+    ndv43 = models.FloatField(db_column='NDV43')  # Field name made lowercase.
+    ndv44 = models.FloatField(db_column='NDV44')  # Field name made lowercase.
+    ndv45 = models.FloatField(db_column='NDV45')  # Field name made lowercase.
+    ndv46 = models.FloatField(db_column='NDV46')  # Field name made lowercase.
+    ndv47 = models.FloatField(db_column='NDV47')  # Field name made lowercase.
+    ndv48 = models.FloatField(db_column='NDV48')  # Field name made lowercase.
+    ndv49 = models.FloatField(db_column='NDV49')  # Field name made lowercase.
+    ndv50 = models.FloatField(db_column='NDV50')  # Field name made lowercase.
+    ndv51 = models.FloatField(db_column='NDV51')  # Field name made lowercase.
+    ndv52 = models.FloatField(db_column='NDV52')  # Field name made lowercase.
+    id_crop_plan = models.IntegerField(blank=True, null=True)
+    id_crop_pixel_result = models.IntegerField(blank=True, null=True)
+    id_field = models.ForeignKey(y2019_List_Of_Fields, models.DO_NOTHING, db_column='id_field')
+    comment = models.CharField(blank=True, null=True, max_length=64)
+
+    class Meta:
+        managed = False
+        db_table = '2019_NDVI_10_1'
+        unique_together = (('x', 'y'))
+        
 
 class m_2022_List_Of_Fields(models.Model):
     id = models.IntegerField(blank=True, primary_key=True)
