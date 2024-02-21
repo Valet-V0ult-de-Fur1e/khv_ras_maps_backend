cbdf1b29bf0654a88cdcb0762fb105a8262fdf4d
Valet-V0ult-de-Fur1e <82310399+Valet-V0ult-de-Fur1e@users.noreply.github.com>
2024-02-21 23:38:07 +0300

    Update mapTwoLayers.js

--- markers/static/mapTwoLayers.js
+++ markers/static/mapTwoLayers.js
@@ -4,8 +4,19 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution:
 const markers1 = JSON.parse(document.getElementById('markers-data-1').textContent);
 const markers2 = JSON.parse(document.getElementById('markers-data-2').textContent);
 // console.log(markers1)
-let feature1 = L.geoJSON(markers2, {style: getStyle()}).bindPopup(function (layer) { return layer.feature.properties.name; }).addTo(map);
-let feature2 = L.geoJSON(markers2, {style: getStyle()}).bindPopup(function (layer) { return layer.feature.properties.name; }).addTo(map);
+let feature1 = L.geoJSON(markers1).bindPopup(function (layer) { return layer.feature.properties.name; }).addTo(map);
+let feature2 = L.geoJSON(markers2, {style: {
+      weight: 0.1,
+      opacity: 0.1,
+      fillColor: 'black',
+      color: 'black',
+      lineJoin: 'miter',
+      fillOpacity: 1,
+      offset: -4,
+      fillColor: "rgb(255, 0, 0)"
+    }
+  }
+).bindPopup(function (layer) { return layer.feature.properties.name; }).addTo(map);
 map.fitBounds(feature1.getBounds(), { padding: [100, 100] });
 
 L.geoJson(markers1)
@@ -23,30 +34,17 @@ L.geoJson(markers1)
   })
   .addTo(map)
 
-  function getStyle() {
-    return {
-        weight: 0.1,
-        opacity: 0.1,
-        fillColor: 'black',
-        color: 'black',
-        lineJoin: 'miter',
-        fillOpacity: 1,
-        offset: -4,
-        fillColor: "rgb(255, 0, 0)"
-    };
-}
-
-  L.geoJson(markers2)
-  .on('click', function(e){
-      var popLocation= e.latlng;
-      var popup = L.popup()
-      .setLatLng(popLocation)
-      .setContent(
-        "<div>polygon id:    " + JSON.stringify(e.sourceTarget.feature["id"]) + "\n" + "</div>"+
-        "<div>area:    " + JSON.stringify(e.sourceTarget.feature.properties["area"]) + "</div>"+
-        "<div>reestr_number:    " + JSON.stringify(e.sourceTarget.feature.properties["reestr_number"]) + "</div>" +
-        "<div>comment:    " + JSON.stringify(e.sourceTarget.feature.properties["comment"]) + "</div>" 
-        )
-      .openOn(map);
-  })
-  .addTo(map)
+L.geoJson(markers2)
+.on('click', function(e){
+    var popLocation= e.latlng;
+    var popup = L.popup()
+    .setLatLng(popLocation)
+    .setContent(
+      "<div>polygon id:    " + JSON.stringify(e.sourceTarget.feature["id"]) + "\n" + "</div>"+
+      "<div>area:    " + JSON.stringify(e.sourceTarget.feature.properties["area"]) + "</div>"+
+      "<div>reestr_number:    " + JSON.stringify(e.sourceTarget.feature.properties["reestr_number"]) + "</div>" +
+      "<div>comment:    " + JSON.stringify(e.sourceTarget.feature.properties["comment"]) + "</div>" 
+      )
+    .openOn(map);
+})
+.addTo(map)
