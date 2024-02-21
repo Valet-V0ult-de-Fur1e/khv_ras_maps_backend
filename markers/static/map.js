410b884d78df7dfb2d1e4b404a88894e66791d78
Valet-V0ult-de-Fur1e <82310399+Valet-V0ult-de-Fur1e@users.noreply.github.com>
2024-02-21 22:09:26 +0300

    Обновление информационных вставок
    update: обновлено отображение информации о поле

--- markers/static/map.js
+++ markers/static/map.js
@@ -1,17 +1,61 @@
 const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-const map = L.map('map')
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution: attribution }).addTo(map);
-const markers = JSON.parse(document.getElementById('markers-data').textContent);
-let feature = L.geoJSON(markers).bindPopup(function (layer) { return layer.feature.properties.name; }).addTo(map);
-map.fitBounds(feature.getBounds(), { padding: [100, 100] });
 
-L.geoJson(markers)
-  .on('click', function(e){
-      var popLocation= e.latlng;
-      var popup = L.popup()
-      .setLatLng(popLocation)
-      .setContent("polygon id:  " + JSON.stringify(e.sourceTarget.feature["id"]))
-      .openOn(map);
-      console.log(e.sourceTarget.feature);
-  })
-  .addTo(map)
+const map = L.map('map')
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution: attribution }).addTo(map);
+
+const markers = JSON.parse(document.getElementById('markers-data').textContent);
+
+let data = new Map();
+
+markers['features'].forEach(element => {
+  if (!data[element.properties['id_crop_sort_fact']]) {
+    data[element.properties['id_crop_sort_fact']] = [];
+  }
+  data[element.properties['id_crop_sort_fact']].push(element);
+});
+
+console.log(data)
+// let feature_list = []
+// data.forEach((values, keys) => {
+//   L.geoJSON(values, {style: getStyle()}).bindPopup(function (layer) { return layer.feature.properties.name; }).addTo(map)
+// }) 
+
+L.geoJSON(data['none'], {style: getStyle()}).bindPopup(function (layer) { return layer.feature.properties.name; }).addTo(map)
+
+console.log(feature_list)
+
+map.fitWorld();
+
+// map.fitBounds(feature_list.getBounds(), { padding: [100, 100] });
+console.log(getRandomArbitrary())
+// console.log(markers)
+
+// L.geoJson(markers)
+//   .on('click', function(e){
+//       var popLocation= e.latlng;
+//       var popup = L.popup()
+//       .setLatLng(popLocation)
+//       .setContent("polygon id:  " + JSON.stringify(e.sourceTarget.feature["id"]))
+//       .openOn(map);
+//       console.log(e.sourceTarget.feature);
+//   })
+//   .addTo(map)
+
+
+function getStyle() {
+  let color_str = "rgb(" + getRandomArbitrary() + ", " + getRandomArbitrary() + ", " + getRandomArbitrary() + ")"
+    return {
+      fillColor: 'black',
+      weight: 2,
+      opacity: 1,
+      color: 'black',  //Outline color
+      fillOpacity: 0.7
+    };
+}
+
+function getRandomArbitrary() {
+  maximum = 255
+  minimum = 0
+  return Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;;
+}
--- markers/static/mapTwoLayers.js
+++ markers/static/mapTwoLayers.js
@@ -1,34 +1,38 @@
 const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 const map = L.map('map')
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution: attribution }).addTo(map);
-// const markers1 = JSON.parse(document.getElementById('markers-data-1').textContent);
+const markers1 = JSON.parse(document.getElementById('markers-data-1').textContent);
 const markers2 = JSON.parse(document.getElementById('markers-data-2').textContent);
+// console.log(markers1)
 let feature1 = L.geoJSON(markers2, {style: getStyle()}).bindPopup(function (layer) { return layer.feature.properties.name; }).addTo(map);
-// let feature2 = L.geoJSON(markers2, {style: getStyle()}).bindPopup(function (layer) { return layer.feature.properties.name; }).addTo(map);
+let feature2 = L.geoJSON(markers2, {style: getStyle()}).bindPopup(function (layer) { return layer.feature.properties.name; }).addTo(map);
 map.fitBounds(feature1.getBounds(), { padding: [100, 100] });
 
-// L.geoJson(markers1)
-//   .on('click', function(e){
-//       var popLocation= e.latlng;
-//       var popup = L.popup()
-//       .setLatLng(popLocation)
-//       .setContent("polygon id:  " + JSON.stringify(e.sourceTarget.feature["id"]))
-//       .openOn(map);
-//       console.log(e.sourceTarget.feature);
-//   })
-//   .addTo(map)
+L.geoJson(markers1)
+  .on('click', function(e){
+      var popLocation= e.latlng;
+      var popup = L.popup()
+      .setLatLng(popLocation)
+      .setContent(
+        "<div>polygon id:    " + JSON.stringify(e.sourceTarget.feature["id"]) + "\n" + "</div>"+
+        "<div>area:    " + JSON.stringify(e.sourceTarget.feature.properties["area"]) + "</div>"+
+        "<div>reestr_number:    " + JSON.stringify(e.sourceTarget.feature.properties["reestr_number"]) + "</div>" +
+        "<div>comment:    " + JSON.stringify(e.sourceTarget.feature.properties["comment"]) + "</div>" 
+        )
+      .openOn(map);
+  })
+  .addTo(map)
 
-  function getStyle(feature) {
+  function getStyle() {
     return {
-        // weight: 0.1,
         weight: 0.1,
-        // opacity: 0.1,
+        opacity: 0.1,
         fillColor: 'black',
         color: 'black',
         lineJoin: 'miter',
         fillOpacity: 1,
         offset: -4,
-        // fillColor: "rgb(255, 0, 0)"
+        fillColor: "rgb(255, 0, 0)"
     };
 }
 
@@ -37,8 +41,12 @@ map.fitBounds(feature1.getBounds(), { padding: [100, 100] });
       var popLocation= e.latlng;
       var popup = L.popup()
       .setLatLng(popLocation)
-      .setContent("polygon id:  " + JSON.stringify(e.sourceTarget.feature["id"]))
+      .setContent(
+        "<div>polygon id:    " + JSON.stringify(e.sourceTarget.feature["id"]) + "\n" + "</div>"+
+        "<div>area:    " + JSON.stringify(e.sourceTarget.feature.properties["area"]) + "</div>"+
+        "<div>reestr_number:    " + JSON.stringify(e.sourceTarget.feature.properties["reestr_number"]) + "</div>" +
+        "<div>comment:    " + JSON.stringify(e.sourceTarget.feature.properties["comment"]) + "</div>" 
+        )
       .openOn(map);
-      console.log(e.sourceTarget.feature);
   })
   .addTo(map)
