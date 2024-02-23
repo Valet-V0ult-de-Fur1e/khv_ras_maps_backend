bf7c7707bd4f2332ec2fe71cd0394161d12b8d72
Valet-V0ult-de-Fur1e <82310399+Valet-V0ult-de-Fur1e@users.noreply.github.com>
2024-02-23 19:05:27 +0300

    code update

--- markers/static/mapAllLayers.js
+++ markers/static/mapAllLayers.js
@@ -1,39 +1,47 @@
 const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 const map = L.map('map')
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution: attribution }).addTo(map);
+let count_of_years = 5
+let data_of_years = new Map();
+for (i = 19; i < 19+count_of_years; ++i) {
+  data_of_years[i] = JSON.parse(document.getElementById('markers-data-' + i).textContent);
+}
 
-const markers1 = JSON.parse(document.getElementById('markers-data-19').textContent);
-const markers2 = JSON.parse(document.getElementById('markers-data-22').textContent);
+let data_of_features = new Map();
 
-let second_year_rgb = getRandomArbitrary() + ", " + getRandomArbitrary() + ", " + getRandomArbitrary()
+for (i = 19; i < 19+count_of_years; ++i) {
+  let year_rgb = getRandomArbitrary() + ", " + getRandomArbitrary() + ", " + getRandomArbitrary()
+  data_of_features[i] = L.geoJSON(data_of_years[i], {style: {fillColor:"rgb(" + year_rgb + ")",fillOpacity: 0.5}})
+}
 
-console.log(second_year_rgb)
-
-let feature1 = L.geoJSON(markers1
-).addTo(map);
-
-let feature2 = L.geoJSON(markers2, {style: {
-  fillColor:"rgb(" + second_year_rgb + ")",
-  fillOpacity: 1
-}}).addTo(map);
+let feature1 = data_of_features[19]
+let list_of_using_layers = [19]
+feature1.addTo(map)
 map.fitBounds(feature1.getBounds(), { padding: [100, 100] });
 
-// L.geoJson(markers1)
-//   .on('click', function(e){
-//       var popLocation= e.latlng;
-//       var popup = L.popup()
-//       .setLatLng(popLocation)
-//       .setContent(
-//         "<div>2019</div>" +
-//         "<div>polygon id:    " + JSON.stringify(e.sourceTarget.feature["id"]) + "\n" + "</div>"+
-//         "<div>area:    " + JSON.stringify(e.sourceTarget.feature.properties["area"]) + "</div>"+
-//         "<div>reestr_number:    " + JSON.stringify(e.sourceTarget.feature.properties["reestr_number"]) + "</div>" +
-//         "<div>comment:    " + JSON.stringify(e.sourceTarget.feature.properties["comment"]) + "</div>" 
-//         )
-//       .openOn(map);
-//   })
-//   .addTo(map)
+const update_button = document.querySelector('updateMap')
+update_button.addEventListener('click', () => {
+  // When there is a "click"
+  // it shows an alert in the browser
+  alert('Oh, you clicked me!')
+})
 
+// let feature2 = data_of_features[22]
+
+
+// for (i = 21; i < 19+count_of_years - 2; ++i) {
+//   L.geoJson(data_of_years[i+1]).on('click', function(e){
+//     var popLocation = e.latlng;
+//     L.popup()
+//     .setLatLng(popLocation)
+//     .setContent(
+//       "<div>20" + i + "</div>" +
+//       "<div>polygon id:    " + JSON.stringify(e.sourceTarget.feature["id"]) + "\n" + "</div>"+
+//       "<div>area:    " + JSON.stringify(e.sourceTarget.feature.properties["area"]) + "</div>"+
+//       "<div>reestr_number:    " + JSON.stringify(e.sourceTarget.feature.properties["reestr_number"]) + "</div>" +
+//       "<div>comment:    " + JSON.stringify(e.sourceTarget.feature.properties["comment"]) + "</div>" 
+//       ).openOn(map);}).addTo(map)
+// }
 
 function getRandomArbitrary() {
   maximum = 255
